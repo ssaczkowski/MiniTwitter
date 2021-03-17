be249aefd2ace6e315eec409c7c6607dfe9402a6
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2021-03-16 23:36:04 -0300

    request and response for login and signup

---
+++ app/src/main/java/com/ssaczkowski/minitwitter/model/RequestSignup.java
@@ -0,0 +1,53 @@
+package com.ssaczkowski.minitwitter.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RequestSignup {
+
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
---
+++ app/src/main/java/com/ssaczkowski/minitwitter/model/ResponseAuth.java
@@ -0,0 +1,75 @@
+package com.ssaczkowski.minitwitter.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseAuth {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("photoUrl")
+    @Expose
+    private String photoUrl;
+    @SerializedName("created")
+    @Expose
+    private String created;
+    @SerializedName("active")
+    @Expose
+    private Boolean active;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+}
