388bd2b91a64ecd4d597328068f1a232aad5de6d
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2021-03-23 14:55:16 -0300

    Fullscreen DialogFragment new tweet

---
+++ app/src/main/java/com/ssaczkowski/minitwitter/ui/NewTweetDialogFragment.java
@@ -0,0 +1,33 @@
+package com.ssaczkowski.minitwitter.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ssaczkowski.minitwitter.R;
+
+
+public class NewTweetDialogFragment extends DialogFragment {
+
+    public NewTweetDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_tweet_dialog, container, false);
+    }
+}
